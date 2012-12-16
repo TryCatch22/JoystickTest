cea886cbc31d99cf4d128f62f61bc79264c816b6
Stephane Beniak <stephane.beniak@gmail.com>
2012-12-16 15:36:50 -0500

    Renamed main class.

--- JoystickTest/JoystickTest/Program.cs
+++ JoystickTest/JoystickTest/Program.cs
@@ -10,7 +10,7 @@ namespace JoystickTest
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (TestGame game = new TestGame())
             {
                 game.Run();
             }
