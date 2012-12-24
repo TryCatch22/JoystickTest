8fe9543cfdc8b2dfa3023c4c598f020b8e4bdb39
Stephane Beniak <stephane.beniak@gmail.com>
2012-12-24 15:32:56 -0500

    Almost forgot some files.

---
+++ GenericGamepad/DirectInputDPad.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.DirectX.DirectInput;
+using Microsoft.Xna.Framework.Input;
+
+namespace GenericGamePad
+{
+	public struct DirectInputDPad
+	{
+		public ButtonState Up;
+		public ButtonState Down;
+		public ButtonState Left;
+		public ButtonState Right;
+
+		public DirectInputDPad(Device device)
+		{
+			Up = ButtonState.Released;
+			Down = ButtonState.Released;
+			Left = ButtonState.Released;
+			Right = ButtonState.Released;
+
+			int direction = device.CurrentJoystickState.GetPointOfView()[0];
+
+			switch (direction)
+			{
+				case (-1):
+					return;
+
+				case (0):
+					Up = ButtonState.Pressed;
+					return;
+
+				case (4500):
+					Up = ButtonState.Pressed;
+					Right = ButtonState.Pressed;
+					return;
+
+				case (9000):
+					Right = ButtonState.Pressed;
+					return;
+
+				case (13500):
+					Right = ButtonState.Pressed;
+					Down = ButtonState.Pressed;
+					return;
+
+				case (18000):
+					Down = ButtonState.Pressed;
+					return;
+
+				case (22500):
+					Down = ButtonState.Pressed;
+					Left = ButtonState.Pressed;
+					return;
+
+				case (27000):
+					Left = ButtonState.Pressed;
+					return;
+
+				case (31500):
+					Left = ButtonState.Pressed;
+					Up = ButtonState.Pressed;
+					return;
+
+				default:
+					return;
+			}
+		}
+	}
+}
---
+++ JoystickTest/JoystickTest/DPadPane.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JoystickTest
+{
+	class DPadPane
+	{
+		public const int Padding = 25;
+
+		private Vector2 boxTopLeft;
+
+		private bool up, down, left, right;
+
+		private Rectangle upBounds, downBounds, leftBounds, rightBounds;
+
+		public DPadPane(Vector2 boxTopLeft)
+		{
+			this.boxTopLeft = boxTopLeft;
+
+			up = down = left = right = false;
+
+			upBounds = new Rectangle((int)boxTopLeft.X + Padding, (int)boxTopLeft.Y, Padding, Padding);
+			downBounds = new Rectangle((int)boxTopLeft.X + Padding, (int)boxTopLeft.Y + 2 * Padding, Padding, Padding);
+			leftBounds = new Rectangle((int)boxTopLeft.X, (int)boxTopLeft.Y + Padding, Padding, Padding);
+			rightBounds = new Rectangle((int)boxTopLeft.X + 2 * Padding, (int)boxTopLeft.Y + Padding, Padding, Padding);
+		}
+
+		public void Update(ButtonState up, ButtonState down, ButtonState left, ButtonState right)
+		{
+			this.up = up == ButtonState.Pressed;
+			this.down = down == ButtonState.Pressed;
+			this.left = left == ButtonState.Pressed;
+			this.right = right == ButtonState.Pressed;
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(TestGame.Pixel, upBounds, up ? Color.Red : Color.Black);
+			spriteBatch.Draw(TestGame.Pixel, downBounds, down ? Color.Red : Color.Black);
+			spriteBatch.Draw(TestGame.Pixel, leftBounds, left ? Color.Red : Color.Black);
+			spriteBatch.Draw(TestGame.Pixel, rightBounds, right ? Color.Red : Color.Black);
+		}
+	}
+}
